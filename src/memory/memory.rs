aa67185581c54a440a49b1c72afe40ac8077a186
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2023-06-16 10:24:02 +0200

    reuse allocator

--- src/memory/memory.rs
+++ src/memory/memory.rs
@@ -2,6 +2,7 @@ use super::allocator::HugePageAllocator;
 use std::alloc::{GlobalAlloc, Layout};
 
 pub struct Memory {
+    allocator: HugePageAllocator,
     pub addr: Option<*mut u8>,
     layout: Option<Layout>,
 }
@@ -9,6 +10,7 @@ pub struct Memory {
 impl Memory {
     pub fn new() -> Self {
         Memory {
+            allocator: HugePageAllocator{},
             addr: None,
             layout: None,
         }
@@ -20,7 +22,7 @@ impl Memory {
         let layout = Layout::array::<char>(size).unwrap();
         let dst: *mut u8;
         unsafe {
-            dst = HugePageAllocator {}.alloc(layout);
+            dst = self.allocator.alloc(layout);
         }
         self.addr = Some(dst);
         self.layout = Some(layout);
@@ -29,7 +31,7 @@ impl Memory {
     pub fn dealloc(&mut self) {
         if let (Some(addr), Some(layout)) = (self.addr, self.layout) {
             unsafe {
-                HugePageAllocator {}.dealloc(addr, layout);
+                self.allocator.dealloc(addr, layout);
             }
         };
     }
