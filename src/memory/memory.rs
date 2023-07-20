0d338825f328439b799586764872b042550efe7c
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2023-07-20 08:33:50 +0200

    Make Memory::alloc err if alloc failed

--- src/memory/memory.rs
+++ src/memory/memory.rs
@@ -1,13 +1,31 @@
+use anyhow::{Context, Error};
+
 use super::allocator::HugePageAllocator;
 use std::{
     alloc::{GlobalAlloc, Layout},
-    mem,
+    fmt, mem,
+    ptr::null_mut,
 };
 
 pub struct Memory {
     allocator: HugePageAllocator,
     pub addr: Option<*mut u8>,
     layout: Option<Layout>,
+}
+
+#[derive(Debug)]
+pub enum MemoryError {
+    AllocFailed,
+}
+
+impl std::error::Error for MemoryError {}
+
+impl fmt::Display for MemoryError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MemoryError::AllocFailed => write!(f, "Allocation failed"),
+        }
+    }
 }
 
 impl Memory {
@@ -17,6 +35,30 @@ impl Memory {
             addr: None,
             layout: None,
         }
+    }
+
+    pub fn initialize(&self, value: u8) -> Result<(), Error> {
+        let layout = self.layout.with_context(|| "layout not initialized")?;
+        let addr = self.addr.with_context(|| "addr not initialized")?;
+        unsafe {
+            for offset in 0..layout.size() {
+                std::ptr::write(addr.add(offset), value);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn check(&self, expected: u8) -> Result<bool, Error> {
+        let layout = self.layout.with_context(|| "layout not initialized")?;
+        let addr = self.addr.with_context(|| "addr not initialized")?;
+        unsafe {
+            for offset in 0..layout.size() {
+                if *addr.add(offset) != expected {
+                    return Ok(false);
+                }
+            }
+        }
+        Ok(true)
     }
 }
 
@@ -29,14 +71,18 @@ impl Memory {
             Some(dst)
         }
     }
-    pub fn alloc(&mut self, size: usize) {
-        let layout = Layout::array::<char>(size).unwrap();
+    pub fn alloc(&mut self, size: usize) -> Result<(), MemoryError> {
+        let layout = Layout::array::<u8>(size).unwrap();
         let dst: *mut u8;
         unsafe {
             dst = self.allocator.alloc(layout);
         }
+        if dst == null_mut() {
+            return Err(MemoryError::AllocFailed);
+        }
         self.addr = Some(dst);
         self.layout = Some(layout);
+        Ok(())
     }
 }
 
