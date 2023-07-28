a98feda10d51db606da642728f68ca09f81e9038
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2023-07-28 15:13:06 +0200

    expose VirtToPhysResolver in mod.rs

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -3,6 +3,7 @@ mod dram_addr;
 mod memory;
 mod timer;
 
+pub use self::allocator::{LinuxPageMap, VirtToPhysResolver};
 pub use self::dram_addr::DRAMAddr;
 pub use self::memory::Memory;
 pub use self::timer::construct_memory_tuple_timer;
