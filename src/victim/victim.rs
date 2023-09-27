00fb7d1981dba6f3604dfc5ec12683a09fd36ac0
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2023-09-27 10:01:53 +0200

    move RSACRT init logic to HammerVictimRsa#init

--- src/victim/victim.rs
+++ src/victim/victim.rs
@@ -3,37 +3,62 @@ use std::ptr::null_mut;
 use anyhow::bail;
 
 use crate::{
-    forge::HammerVictim, memory::Memory, RSACRT_alloc, RSACRT_check_openssl_version, RSACRT_ctx_t,
-    RSACRT_free_ctx, RSACRT_init, RSACRT_sign, RSACRT_verify,
+    forge::HammerVictim, memory::Memory, RSACRT_alloc, RSACRT_check_dmp1,
+    RSACRT_check_openssl_version, RSACRT_ctx_t, RSACRT_free_ctx, RSACRT_get_dmp1, RSACRT_init,
+    RSACRT_sign, RSACRT_verify, BIGNUM,
 };
 
 #[derive(Debug)]
 pub struct HammerVictimRsa {
     ctx: *mut RSACRT_ctx_t,
+    d: *mut libc::c_ulong,
+    exp_dmp1: Option<*const BIGNUM>,
 }
 
-impl<'a> HammerVictimRsa {
-    pub fn new(memory: &'a Memory, offset: usize) -> anyhow::Result<Self> {
+impl HammerVictimRsa {
+    pub fn new(memory: &Memory, offset: usize) -> anyhow::Result<Self> {
         let check = unsafe { RSACRT_check_openssl_version() };
         if check != 1 {
             bail!("check version");
         }
-        let (ctx, ret) = unsafe {
-            let d = memory.addr.add(offset) as *mut libc::c_ulong;
+        let ctx = unsafe {
             let mut ctx: *mut RSACRT_ctx_t = null_mut();
             RSACRT_alloc(&mut ctx);
-
-            (ctx, RSACRT_init(d, ctx))
+            ctx
         };
-        if ret != 0 {
-            bail!("RSACRT_init");
-        }
-        Ok(HammerVictimRsa { ctx })
+        Ok(HammerVictimRsa {
+            ctx,
+            d: unsafe { memory.addr.add(offset) as *mut libc::c_ulong },
+            exp_dmp1: None,
+        })
     }
 }
 
 impl HammerVictim for HammerVictimRsa {
+    fn init(&mut self) {
+        let ret = unsafe {
+            let ctx_size = std::mem::size_of::<RSACRT_ctx_t>();
+            info!(
+                "Put RSA context at address 0x{:02X}..0x{:02X}",
+                self.d as usize,
+                self.d.add(ctx_size) as usize
+            );
+            RSACRT_init(self.d, self.ctx)
+        };
+        if ret != 0 {
+            panic!("RSACRT_init");
+        }
+        unsafe {
+            let mut dmp1 = null_mut() as *mut BIGNUM;
+            RSACRT_get_dmp1(self.ctx, &mut dmp1 as *mut *mut BIGNUM);
+            self.exp_dmp1 = Some(dmp1);
+        };
+    }
+
     fn check(&mut self) -> bool {
+        //check dmp1 correct
+        let ret = unsafe { RSACRT_check_dmp1(self.ctx, self.exp_dmp1.unwrap()) };
+        return ret != 0;
         let msg = "hello world";
         let mut sig = std::mem::MaybeUninit::uninit();
         let mut siglen = std::mem::MaybeUninit::uninit();
@@ -55,7 +80,7 @@ impl HammerVictim for HammerVictimRsa {
                 msg.as_ptr(),
                 msg.len(),
                 sig.assume_init() as *const u8,
-                siglen.assume_init() as usize,
+                siglen.assume_init(),
             )
         };
         ret != 1
