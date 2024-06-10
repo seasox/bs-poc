02ac21b8e42cdf9ef0350a84fe5bc5745c4b5b34
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-06-10 13:58:24 +0200

    build and bindgen ptedit, refactor build.rs

--- build.rs
+++ build.rs
@@ -3,71 +3,105 @@ use std::path::PathBuf;
 
 use glob::glob;
 
-fn main() {
-    // Tell cargo to tell rustc to link the rsa_crt and crypto libraries
-    println!("cargo:rustc-link-lib=rsa_crt");
-    println!("cargo:rustc-link-lib=crypto");
+fn bind_spoiler(bindings: bindgen::Builder) -> bindgen::Builder {
+    bindings.header("lib/spoiler/include/spoiler.h")
+}
 
-    // Tell cargo to invalidate the built crate whenever the RSA lib or OpenSSL changes
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rerun-if-changed=lib/rsa/rsa_crt.c");
-    println!("cargo:rerun-if-changed=rsa_crt.h");
+fn build_spoiler() {
+    // build spoiler
+    let spoiler_srcs = glob("lib/spoiler/src/*.c")
+        .expect("Failed to glob lib/spoiler/src")
+        .filter_map(Result::ok);
+    let spoiler_incs = glob("lib/spoiler/include/*.h")
+        .expect("Failed to glob lib/spoiler/include")
+        .filter_map(Result::ok);
+    cc::Build::new()
+        .files(spoiler_srcs)
+        .files(spoiler_incs)
+        .flag("-g")
+        .flag("-O0")
+        .compile("libspoiler.a");
+}
+
+fn bind_rsa(bindings: bindgen::Builder) -> bindgen::Builder {
+    println!("cargo:rerun-if-changed=lib/rsa/rsa_crt.h");
+    // Link with OpenSSL library
     println!("cargo:rerun-if-env-changed=OPENSSL_LIB_DIR");
+    if let Ok(lib_dir) = std::env::var("OPENSSL_LIB_DIR") {
+        println!("cargo:rustc-link-search=native={}", lib_dir);
+    }
     println!("cargo:rerun-if-env-changed=OPENSSL_INCLUDE_DIR");
+    if let Ok(include_dir) = std::env::var("OPENSSL_INCLUDE_DIR") {
+        println!("cargo:include={}", include_dir);
+    }
+    bindings.header("lib/rsa/rsa_crt.h")
+}
 
-    let mut bindings = bindgen::Builder::default();
-
+fn build_rsa() {
     // build rsa-crt
     cc::Build::new()
         .file("lib/rsa/rsa_crt.c")
         .include("/usr/include")
         .flag("-Wno-deprecated-declarations")
         .compile("librsa_crt.a");
+    // Tell cargo to tell rustc to link the rsa_crt and crypto libraries
+    println!("cargo:rustc-link-lib=rsa_crt");
+    println!("cargo:rustc-link-lib=crypto");
+    println!("cargo:rerun-if-changed=lib/rsa/rsa_crt.c");
+}
 
-    bindings = bindings.header("lib/rsa/rsa_crt.h");
+fn bind_ptedit(bindings: bindgen::Builder) -> bindgen::Builder {
+    println!("cargo:rerun-if-changed=lib/ptedit/ptedit.h");
+    bindings.header("lib/ptedit/ptedit.h")
+}
 
-    if cfg!(spoiler) {
-        // build spoiler
-        let spoiler_srcs = glob("lib/spoiler/src/*.c")
-            .expect("Failed to glob lib/spoiler/src")
-            .filter_map(Result::ok);
-        let spoiler_incs = glob("lib/spoiler/include/*.h")
-            .expect("Failed to glob lib/spoiler/include")
-            .filter_map(Result::ok);
-        cc::Build::new()
-            .files(spoiler_srcs)
-            .files(spoiler_incs)
-            .flag("-g")
-            .flag("-O0")
-            .compile("libspoiler.a");
-        bindings = bindings.header("lib/spoiler/include/spoiler.h");
-    }
+fn build_ptedit() {
+    // build ptedit
+    cc::Build::new()
+        .file("lib/ptedit/ptedit.c")
+        .compile("libptedit.a")
+}
 
-    // Link with OpenSSL library
-
-    if let Ok(lib_dir) = std::env::var("OPENSSL_LIB_DIR") {
-        println!("cargo:rustc-link-search=native={}", lib_dir);
-    }
-
-    if let Ok(include_dir) = std::env::var("OPENSSL_INCLUDE_DIR") {
-        println!("cargo:include={}", include_dir);
-    }
-
+fn run_bindgen(bindings: bindgen::Builder) -> bindgen::Bindings {
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
-    let bindings = bindings
+    bindings
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
+        .expect("Unable to generate bindings")
+}
 
+fn write_bindings(bindings: bindgen::Bindings) {
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    let mut bindings = bindgen::Builder::default();
+
+    bindings = bind_rsa(bindings);
+
+    if cfg!(spoiler) {
+        bindings = bind_spoiler(bindings);
+    }
+
+    bindings = bind_ptedit(bindings);
+
+    let bindings = run_bindgen(bindings);
+
+    build_rsa();
+    if cfg!(spoiler) {
+        build_spoiler()
+    }
+    build_ptedit();
+
+    write_bindings(bindings);
 }
