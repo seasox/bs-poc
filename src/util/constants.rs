f904df3a1249e89c7d3d0853d434fe53fc8d0d9f
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-06-21 15:51:27 +0200

    add PAGE_SHIFT constant

--- src/util/constants.rs
+++ src/util/constants.rs
@@ -1,4 +1,6 @@
 pub const KB: usize = 1 << 10;
 pub const MB: usize = 1 << 20;
 
-pub const PAGE_SIZE: usize = 4096;
+pub const PAGE_SHIFT: usize = 12;
+
+pub const PAGE_SIZE: usize = 1 << PAGE_SHIFT;
