acbc168310cb240d67eab7039e055a79a49375b3
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-06-21 15:47:37 +0200

    Add HugepageAllocator to memory module

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -5,10 +5,10 @@ mod memblock;
 mod memory;
 mod timer;
 
-pub use self::allocator::{LinuxPageMap, VirtToPhysResolver};
+pub use self::allocator::{LinuxPageMap, HugepageAllocator, VirtToPhysResolver};
 pub use self::consec_checker::*;
 pub use self::dram_addr::DRAMAddr;
 pub use self::memblock::*;
 pub use self::memory::Memory;
 pub use self::memory::VictimMemory;
-pub use self::timer::construct_memory_tuple_timer;
+pub use self::timer::{construct_memory_tuple_timer, MemoryTupleTimer};
