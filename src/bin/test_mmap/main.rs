75dbd37d5ac03313fec498e339f0b70d82819407
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-07-26 13:47:14 +0200

    remove dead code

--- src/bin/test_mmap/main.rs
+++ src/bin/test_mmap/main.rs
@@ -1,11 +1,9 @@
-use std::ptr::null_mut;
-
 use anyhow::{bail, Context};
 use bs_poc::{
     memory::{
         construct_memory_tuple_timer, AllocChecker, ConsecCheckBankTiming, ConsecCheckPfn, MemBlock,
     },
-    util::{BlacksmithConfig, MemConfiguration, KB, MB},
+    util::{BlacksmithConfig, MemConfiguration, MB},
 };
 use clap::Parser;
 use log::{error, info};
@@ -17,7 +15,7 @@ struct CliArgs {
     config: String,
 }
 
-fn mmap_1g_hugepage() -> anyhow::Result<*mut libc::c_void> {
+fn _mmap_1g_hugepage() -> anyhow::Result<*mut libc::c_void> {
     const ADDR: *mut libc::c_void = 0x2000000000 as *mut libc::c_void;
     let ptr = unsafe {
         libc::mmap(
@@ -64,32 +62,6 @@ fn mmap(size: usize) -> anyhow::Result<*mut libc::c_void> {
     Ok(ptr)
 }
 
-fn format_arr(arr: &[u64; 11]) -> String {
-    let mut s = String::new();
-    for i in 0..11 {
-        s.push_str(&format!("{:5}", arr[i]));
-    }
-    s
-}
-
-fn pti_locked_blocks() -> anyhow::Result<[u64; 11]> {
-    let pti = match lpfs::proc::pagetypeinfo::pagetypeinfo() {
-        Ok(pti) => pti,
-        Err(e) => bail!("{:?}", e),
-    };
-    let pages = pti.free_pages().clone();
-    let mut free_pages = [0_u64; 11];
-    for (_, zone, migrate_type, blocks) in pages {
-        if zone == "Normal" && (migrate_type == "Movable" || migrate_type == "Reclaimable") {
-            continue;
-        }
-        for i in 0..11 {
-            free_pages[i] += blocks[i];
-        }
-    }
-    Ok(free_pages)
-}
-
 unsafe fn _main() -> anyhow::Result<()> {
     env_logger::init();
     let args = CliArgs::parse();
@@ -97,27 +69,15 @@ unsafe fn _main() -> anyhow::Result<()> {
     let config = BlacksmithConfig::from_jsonfile(&args.config).with_context(|| "from_jsonfile")?;
     let mem_config =
         MemConfiguration::from_bitdefs(config.bank_bits, config.row_bits, config.col_bits);
-    let mut checker = ConsecCheckBankTiming::new(mem_config, &*timer, config.threshold);
+    let mut checker = ConsecCheckBankTiming::new(mem_config, timer, config.threshold);
     let mut pfn_checker = ConsecCheckPfn {};
     info!("Let's go 🚀");
     loop {
         const SIZE: usize = 4 * MB;
-        //let dummy = mmap(1024 * MB)?;
         let mut allocations = Vec::with_capacity(3000);
         for i in 0..3000 {
-            //let buddy_before = get_normal_page_nums().expect("no buddy info");
-            //allocations[i] = mmap(SIZE)?;
-            //let mut block = MemBlock::new(mmap_1g_hugepage()? as *mut u8, 1024 * MB);
             let block = MemBlock::new(mmap(SIZE)? as *mut u8, SIZE);
             allocations.push(block.clone());
-            //let buddy_after = get_normal_page_nums().expect("no buddy info");
-            /*let diff = diff_arrs(&buddy_before, &buddy_after);
-            let sum = diff.iter().enumerate().fold(0, |acc, (i, x)| {
-                acc + x * 2_i64.pow(i as u32) * 4 * KB as i64
-            });*/
-            // Problem mit diesem Ansatz: offset zwischen virtueller und physikalischer Adresse ist konsistent.
-            // Muss im address lookup was rumpatchen um das Phänomen zu simulieren.
-
             let pfn_check = pfn_checker.check(&block)?;
             if pfn_check {
                 let is_consec = checker.check(&block)?;
@@ -128,24 +88,12 @@ unsafe fn _main() -> anyhow::Result<()> {
                 }
                 break;
             }
-            //file.write_all(format!("{},{}\n", sum, pfn_consec).as_bytes())?;
         }
         for allocation in allocations {
             allocation.dealloc();
         }
-        //libc::munmap(dummy, 1024 * MB);
     }
 }
-
-/*unsafe fn _main() -> anyhow::Result<()> {
-    env_logger::init();
-    loop {
-        let pti = pti_locked_blocks()?;
-        info!("Locked blocks (PTI):   {}", format_arr(&pti));
-        let locked = all_locked_blocks()?;
-        info!("Locked blocks (buddy): {}", format_arr(&locked));
-    }
-}*/
 
 fn main() -> anyhow::Result<()> {
     unsafe { _main() }
