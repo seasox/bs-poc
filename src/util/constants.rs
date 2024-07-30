f6523f441b42db31d5804c1b9c746f752dfe6c7f
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-07-30 16:49:49 +0200

    add ROW_SHIFT

--- src/util/constants.rs
+++ src/util/constants.rs
@@ -2,8 +2,9 @@ pub const KB: usize = 1 << 10;
 pub const MB: usize = 1 << 20;
 
 pub const PAGE_SHIFT: usize = 12;
+pub const ROW_SHIFT: usize = 13;
 
-pub const ROW_SIZE: usize = 8192;
 pub const PAGE_SIZE: usize = 1 << PAGE_SHIFT;
+pub const ROW_SIZE: usize = 1 << ROW_SHIFT;
 
 pub const TIMER_ROUNDS: usize = 100_000;
