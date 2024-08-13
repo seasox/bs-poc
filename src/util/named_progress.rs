c1bc0c772dc2b1f49fc114b1a6567b8a8c1297d6
Jeremy Boy <jeremy.boy@student.uni-luebeck.de>
2024-08-13 14:55:22 +0200

    fixed spacing between progress bar labels and progress bar

--- src/util/named_progress.rs
+++ src/util/named_progress.rs
@@ -6,9 +6,11 @@ pub trait NamedProgress {
 
 impl NamedProgress for ProgressStyle {
     fn named_bar(name: &str) -> Self {
-        let fmt = name.to_string();
-        let fmt = fmt
-            + " {bar:40.cyan/blue} {pos:>6}/{len:<6} [{elapsed_precise} ({eta} remaining)] {msg}";
+        let mut fmt = name.to_string();
+        for _ in 0..(10 - name.len() - 1) {
+            fmt += " ";
+        }
+        fmt += " {bar:40.cyan/blue} {pos:>6}/{len:<6} [{elapsed_precise} ({eta} remaining)] {msg}";
         ProgressStyle::default_bar()
             .template(&fmt)
             .unwrap_or(ProgressStyle::default_bar())
