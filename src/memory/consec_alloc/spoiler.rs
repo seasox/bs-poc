fc04cff14d1ab9ebac84f83e2be5e0c9f03a750a
Jeremy Boy <je.boy@uni-luebeck.de>
2024-09-13 11:39:35 +0200

    munmap unneeded regions of search buffer after running spoiler attack

--- src/memory/consec_alloc/spoiler.rs
+++ src/memory/consec_alloc/spoiler.rs
@@ -1,12 +1,13 @@
 use crate::memory::memblock::FormatPfns;
-use crate::memory::util::compact_mem;
+use crate::memory::util::{compact_mem, mmap, munmap};
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::ptr::null_mut;
 
 use itertools::Itertools;
 
-use crate::util::{KB, MB};
-use crate::{length, memory_addresses, retry};
+use crate::util::MB;
+use crate::{addr_space, length, memory_addresses, retry};
 use crate::{
     memory::{ConsecBlocks, MemBlock},
     util::PAGE_SIZE,
@@ -19,6 +20,23 @@ pub struct ConsecAllocSpoiler {}
 impl ConsecAllocSpoiler {
     pub fn new() -> Self {
         Self {}
+    }
+}
+
+/// Trait for converting raw pointers to Rust types
+trait FromRaw<T> {
+    /// Convert a raw pointer to a Rust type
+    fn from_raw(raw_ptr: *mut T) -> Self;
+}
+
+type AddrSpace = Vec<*mut u8>;
+impl FromRaw<addr_space> for AddrSpace {
+    fn from_raw(raw_ptr: *mut addr_space) -> Self {
+        let addrs = unsafe { memory_addresses(raw_ptr) };
+        let addrs_len = unsafe { length(raw_ptr) } as usize;
+
+        let vec = unsafe { Vec::from_raw_parts(addrs, addrs_len, addrs_len) };
+        vec
     }
 }
 
@@ -38,53 +56,49 @@ impl ConsecAllocator for ConsecAllocSpoiler {
         let mut blocks = vec![];
         const BLOCK_SIZE: usize = 8 * MB;
         for _ in 0..size.div_ceil(BLOCK_SIZE) {
-            let addr_space = retry!(|| {
+            let search_buffer_size = PAGE_COUNT * PAGE_SIZE;
+            let addrs_blocks = retry!(|| {
                 compact_mem()?;
-                let search_buffer = unsafe {
-                    let ptr = libc::mmap(
-                        std::ptr::null_mut(),
-                        PAGE_COUNT * PAGE_SIZE,
-                        libc::PROT_READ | libc::PROT_WRITE,
-                        libc::MAP_POPULATE | libc::MAP_ANONYMOUS | libc::MAP_PRIVATE,
-                        -1,
-                        0,
-                    );
-                    if ptr == libc::MAP_FAILED {
-                        panic!("Failed to mmap");
-                    }
-                    ptr as *mut u8
-                };
-                let a = auto_spoiler(search_buffer);
-                if a.is_null() {
-                    libc::munmap(search_buffer as *mut libc::c_void, PAGE_COUNT * PAGE_SIZE);
-                    Err(anyhow::anyhow!("Failed to get address space"))
-                } else {
-                    let base = MemBlock::new(search_buffer, 512 * MB);
-                    let pfns = base.consec_pfns()?.format_pfns();
-                    info!("PFN ranges: {}", pfns);
-                    let mut f = OpenOptions::new()
-                        .create(true)
-                        .append(true)
-                        .open("pfns.txt")
-                        .expect("Failed to open pfns.txt");
-                    writeln!(f, "PFN ranges:\n{}", pfns).expect("Failed to write to pfns.txt");
-                    Ok(a)
+                let search_buffer = mmap(null_mut(), search_buffer_size);
+                let addr_space = auto_spoiler(search_buffer);
+                if addr_space.is_null() {
+                    libc::munmap(search_buffer as *mut libc::c_void, search_buffer_size);
+                    return Err(anyhow::anyhow!("Failed to get address space"));
                 }
+                let base = MemBlock::new(search_buffer, 512 * MB);
+                let pfns = base.consec_pfns()?.format_pfns();
+                debug!("PFN ranges: {}", pfns);
+                let mut f = OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open("pfns.txt")
+                    .expect("Failed to open pfns.txt");
+                writeln!(f, "PFN ranges:\n{}", pfns).expect("Failed to write to pfns.txt");
+
+                let addrs = AddrSpace::from_raw(addr_space);
+
+                let to_munmap = (0..512 * MB)
+                    .step_by(PAGE_SIZE)
+                    .map(|i| search_buffer.byte_add(i))
+                    .filter(|ptr| !addrs.contains(&ptr))
+                    .collect_vec();
+
+                for ptr in to_munmap {
+                    munmap(ptr, PAGE_SIZE);
+                }
+
+                for &addr in &addrs {
+                    unsafe { libc::memset(addr as *mut libc::c_void, 0x11, PAGE_SIZE) };
+                }
+
+                let addrs_blocks = addrs
+                    .into_iter()
+                    .map(|addr| MemBlock::new(addr, PAGE_SIZE))
+                    .collect_vec();
+                Ok(addrs_blocks)
             });
-
-            let addrs = memory_addresses(addr_space);
-            let addrs_len = length(addr_space) as usize;
-
-            // TODO munmap search_buffer \setminus addrs
-
-            let addrs_blocks = (0..addrs_len)
-                .map(|i| MemBlock::new(*addrs.add(i), 4 * KB))
-                .collect_vec();
-            let cons = ConsecBlocks::new(addrs_blocks.clone());
-            cons.log_pfns();
             blocks.extend(addrs_blocks);
         }
-
         Ok(ConsecBlocks { blocks })
     }
 }
