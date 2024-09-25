0e85c24b3af149edc5fb87a7e2226d2c2f8f8aa8
Jeremy Boy <je.boy@uni-luebeck.de>
2024-09-25 17:39:46 +0200

    docs: add remark for ConsecAlloc enum

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -2,7 +2,7 @@
 //!
 //! This module provides different memory allocation strategies for allocating consecutive memory blocks. The strategies include buddy allocation, CoCo, hugepage allocation, mmap, and spoiler.
 //!
-//! To add a new memory allocation strategy, implement the `ConsecAllocator` trait for the new strategy.
+//! To add a new memory allocation strategy, implement the `ConsecAllocator` trait for the new strategy and add a new variant to the `ConsecAlloc` enum.
 pub mod buddyinfo;
 pub mod coco;
 pub mod hugepage;
