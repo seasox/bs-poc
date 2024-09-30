bc6387acdade2e3fc1b735928aa4f8b90f12b586
Jeremy Boy <je.boy@uni-luebeck.de>
2024-09-30 15:07:02 +0200

    fix: overflow in ProgressStyle::named_bar

--- src/util/named_progress.rs
+++ src/util/named_progress.rs
@@ -7,7 +7,7 @@ pub trait NamedProgress {
 impl NamedProgress for ProgressStyle {
     fn named_bar(name: &str) -> Self {
         let mut fmt = name.to_string();
-        for _ in 0..(10 - name.len() - 1) {
+        for _ in 0..(10 - name.len() as i64 - 1) {
             fmt += " ";
         }
         fmt += " {bar:40.cyan/blue} {pos:>6}/{len:<6} [{elapsed_precise} ({eta} remaining)] {msg}";
