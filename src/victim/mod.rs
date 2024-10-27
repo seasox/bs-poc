fd661a23080b0dffac9168aac72bbf4d40e0aae3
Jeremy Boy <je.boy@uni-luebeck.de>
2024-10-27 19:10:23 +0100

    chore: doc, fix imports in victim/mod.rs

--- src/victim/mod.rs
+++ src/victim/mod.rs
@@ -3,9 +3,11 @@
 //! A victim is the target of the Rowhammer attack. It can be a memory region or a process. To probe memory regions, the `MemCheck` victim is used. To probe a victim process, the `Process` victim is used.
 pub mod mem_check;
 pub mod process;
+pub mod stack_process;
 
 pub use self::mem_check::HammerVictimMemCheck as MemCheck;
 pub use self::process::VictimProcess as Process;
+pub use self::stack_process::StackProcess;
 
 /// The HammerVictim trait. A victim must implement this trait to be used as a target for the hammer attack.
 ///
@@ -14,7 +16,7 @@ pub use self::process::VictimProcess as Process;
 pub trait HammerVictim<T> {
     /// Initialize the victim. This method is called before the hammering starts.
     fn init(&mut self) {}
-    /// Check if the hammering was successful. Returns Some with an optional string describing the result if the hammering was successful, None otherwise.
+    /// Check if the hammering was successful. Returns Some with an optional value of type T describing the result if the hammering was successful, None otherwise.
     fn check(&mut self) -> Option<T>;
     /// Stop the victim. This method is called after the hammering is done. This consumes the victim.
     fn stop(self);
