2e41418b4ecbd918d26183ad3e0e8113056e3f5e
Jeremy Boy <je.boy@uni-luebeck.de>
2024-11-22 15:39:02 +0100

    chore: dont store flips in HammerVictimMemCheck: we have adapted the trait s.t. check can have a return value

--- src/victim/mem_check.rs
+++ src/victim/mem_check.rs
@@ -6,15 +6,17 @@ use crate::victim::HammerVictim;
 pub struct HammerVictimMemCheck<'a> {
     memory: &'a dyn VictimMemory,
     seed: Option<[u8; 32]>,
-    flips: Vec<BitFlip>,
 }
 
 impl<'a> HammerVictimMemCheck<'a> {
     pub fn new(memory: &'a dyn VictimMemory) -> Self {
+        HammerVictimMemCheck { memory, seed: None }
+    }
+
+    pub fn new_with_seed(memory: &'a dyn VictimMemory, seed: [u8; 32]) -> Self {
         HammerVictimMemCheck {
             memory,
-            seed: None,
-            flips: vec![],
+            seed: Some(seed),
         }
     }
 }
@@ -27,22 +29,15 @@ impl<'a> HammerVictim<Vec<BitFlip>> for HammerVictimMemCheck<'a> {
     }
 
     fn check(&mut self) -> anyhow::Result<Vec<BitFlip>> {
-        self.flips = self
+        let flips = self
             .memory
             .check(self.seed.with_context(|| "no seed").unwrap());
-        if !self.flips.is_empty() {
-            Ok(self.flips.clone())
+        if !flips.is_empty() {
+            Ok(flips.clone())
         } else {
             bail!("No flips detected")
         }
     }
 
-    fn stop(self) {
-        let virt_addrs: Vec<String> = self
-            .flips
-            .iter()
-            .map(|bf| format!("{:?}", bf.addr,))
-            .collect();
-        info!("Addresses with flips: {:?}", virt_addrs);
-    }
+    fn stop(self) {}
 }
