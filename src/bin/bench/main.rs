bfeac3ad32429396ab27949290d2213c479045ff
Jeremy Boy <je.boy@uni-luebeck.de>
2025-01-09 16:11:53 +0100

    feat: hammer benchmark

---
+++ src/bin/bench/main.rs
@@ -0,0 +1,285 @@
+use std::{
+    fs::File,
+    io::{stdin, BufReader},
+    time::Duration,
+};
+
+use anyhow::bail;
+use bs_poc::{
+    allocator::{
+        self, hugepage::HugepageAllocator, BuddyInfo, CoCo, ConsecAlloc, ConsecAllocator,
+        HugepageRandomized, Mmap, Pfn, Spoiler,
+    },
+    hammerer::{
+        blacksmith::{
+            blacksmith_config::BlacksmithConfig,
+            hammerer::{FuzzSummary, HammeringPattern, PatternAddressMapper},
+            jitter::AggressorPtr,
+        },
+        make_hammer, HammerStrategy, Hammerer, Hammering,
+    },
+    memory::{
+        mem_configuration::MemConfiguration, BitFlip, ConsecBlocks, ConsecCheck,
+        ConsecCheckBankTiming, DataPattern,
+    },
+    retry, victim,
+};
+use clap::Parser;
+use indicatif::{MultiProgress, ProgressBar};
+use indicatif_log_bridge::LogWrapper;
+use log::{info, warn};
+use rand::{rngs::StdRng, SeedableRng};
+
+/// CLI arguments for the `bench` binary.
+///
+/// This struct defines the command line arguments that can be passed to the `hammer` binary.
+#[derive(Debug, Parser)]
+struct CliArgs {
+    ///The `blacksmith` config file.
+    #[clap(long = "config", default_value = "config/bs-config.json")]
+    config: String,
+    /// The JSON file containing hammering patterns to load.
+    #[clap(long = "load-json", default_value = "config/fuzz-summary.json")]
+    load_json: String,
+    /// The pattern ID to load from the `blacksmith` JSON file.
+    #[clap(
+        long = "pattern",
+        default_value = "39ad622b-3bfe-4161-b860-dad5f3e6dd68"
+    )]
+    pattern: Option<String>,
+    /// The mapping ID to load from the `blacksmith` JSON file. Optional argument, will determine most optimal pattern if omitted.
+    #[clap(long = "mapping")]
+    mapping: Option<String>,
+    /// The hammering strategy to use.
+    #[clap(long = "hammerer", default_value = "blacksmith")]
+    hammerer: HammerStrategy,
+    /// The number of rounds for benchmarking
+    #[arg(long, default_value = "1000")]
+    repeat: usize,
+}
+
+/// The type of allocation strategy to use.
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum AllocStrategy {
+    /// Use `/proc/buddyinfo` to monitor availability of page orders, assume consecutive memory according to the delta in buddyinfo.
+    BuddyInfo,
+    // Allocate using the CoCo dec mem module: https://git.its.uni-luebeck.de/research-projects/tdx/kmod-coco-dec-mem
+    CoCo,
+    /// Allocate consecutive memory using huge pages.
+    Hugepage,
+    /// Allocate consecutive memory using huge pages with randomization. This will return random 4 MB chunks of a 1 GB hugepage.
+    HugepageRnd,
+    /// Allocate consecutive memory using `bank timing`. This will `mmap` a large buffer and find consecutive memory using bank timing check
+    BankTiming,
+    /// Allocate a large block of memory and use pagemap to find consecutive blocks
+    Pfn,
+    /// Allocate consecutive memory using the Spoiler attack. This strategy will measure read-after-write pipeline conflicts to determine consecutive memory.
+    Spoiler,
+}
+
+impl AllocStrategy {
+    fn create_allocator(
+        &self,
+        mem_config: MemConfiguration,
+        conflict_threshold: u64,
+        progress: Option<MultiProgress>,
+    ) -> allocator::ConsecAlloc {
+        match self {
+            AllocStrategy::BuddyInfo => ConsecAlloc::BuddyInfo(BuddyInfo::new(
+                ConsecCheck::BankTiming(ConsecCheckBankTiming::new(mem_config, conflict_threshold)),
+            )),
+            AllocStrategy::CoCo => ConsecAlloc::CoCo(CoCo {}),
+            AllocStrategy::BankTiming => ConsecAlloc::Mmap(Mmap::new(
+                ConsecCheck::BankTiming(ConsecCheckBankTiming::new(mem_config, conflict_threshold)),
+                progress,
+            )),
+            AllocStrategy::Hugepage => ConsecAlloc::Hugepage(HugepageAllocator::default()),
+            AllocStrategy::HugepageRnd => ConsecAlloc::HugepageRnd(HugepageRandomized::new(1)),
+            AllocStrategy::Pfn => ConsecAlloc::Pfn(Pfn::default()),
+            AllocStrategy::Spoiler => ConsecAlloc::Spoiler(Box::new(Spoiler::new(
+                mem_config,
+                conflict_threshold,
+                progress,
+            ))),
+        }
+    }
+}
+
+fn cli_ask_pattern(json_filename: String) -> anyhow::Result<String> {
+    let f = File::open(&json_filename)?;
+    let reader = BufReader::new(f);
+    let fuzz: FuzzSummary = serde_json::from_reader(reader)?;
+    let pattern = retry!(|| {
+        println!("Please choose a pattern:");
+        for (i, pattern) in fuzz.hammering_patterns.iter().enumerate() {
+            let best_mapping = pattern
+                .determine_most_effective_mapping()
+                .expect("no mappings");
+            println!(
+                "{}: {} (best mapping {} with {} flips)",
+                i,
+                pattern.id,
+                best_mapping.id,
+                best_mapping.count_bitflips()
+            )
+        }
+        let mut option = String::new();
+        stdin()
+            .read_line(&mut option)
+            .expect("Did not enter a correct string");
+        match str::parse::<usize>(option.trim()) {
+            Ok(i) => {
+                if i < fuzz.hammering_patterns.len() {
+                    return Ok(fuzz.hammering_patterns[i].id.clone());
+                }
+                bail!(
+                    "Invalid pattern index {}/{}",
+                    i,
+                    fuzz.hammering_patterns.len()
+                );
+            }
+            Err(e) => Err(e.into()),
+        }
+    });
+    Ok(pattern)
+}
+
+struct LoadedPattern {
+    pattern: HammeringPattern,
+    mapping: PatternAddressMapper,
+}
+
+fn load_pattern(args: &CliArgs) -> anyhow::Result<LoadedPattern> {
+    // load patterns from JSON
+    let pattern = match &args.pattern {
+        Some(pattern) => pattern.clone(),
+        None => cli_ask_pattern(args.load_json.clone())?,
+    };
+
+    let pattern = HammeringPattern::load_pattern_from_json(args.load_json.clone(), pattern)?;
+    let mapping = match &args.mapping {
+        Some(mapping) => pattern.find_mapping(mapping).expect("mapping not found"),
+        None => pattern
+            .determine_most_effective_mapping()
+            .expect("pattern contains no mapping"),
+    };
+
+    info!("Using mapping {}", mapping.id);
+    let max_flips = mapping.count_bitflips();
+    info!("Max flips: {:?}", max_flips);
+    if max_flips == 0 {
+        bail!("No flips in mapping");
+    }
+    info!("Flips in mapping: {:?}", &mapping.bit_flips);
+    Ok(LoadedPattern { pattern, mapping })
+}
+
+pub fn init_logging_with_progress() -> anyhow::Result<MultiProgress> {
+    let logger =
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).build();
+    let progress = MultiProgress::new();
+    LogWrapper::new(progress.clone(), logger).try_init()?;
+    Ok(progress)
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct RoundProfile {
+    bit_flips: Vec<BitFlip>,
+    pattern: DataPattern,
+    duration: Duration,
+}
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Profiling {
+    rounds: Vec<RoundProfile>,
+}
+
+unsafe fn _main() -> anyhow::Result<()> {
+    let progress = init_logging_with_progress()?;
+
+    // parse args
+    let args = CliArgs::parse();
+    let config = BlacksmithConfig::from_jsonfile(&args.config)?;
+    let mem_config = MemConfiguration::from_blacksmith(&config);
+    let pattern = load_pattern(&args)?;
+
+    info!("Args: {:?}", args);
+
+    let mut alloc_strategy = ConsecAlloc::Pfn(Pfn::default());
+    let block_size = alloc_strategy.block_size();
+
+    let memory = allocator::alloc_memory(&mut alloc_strategy, mem_config, &pattern.mapping)?;
+
+    let hammerer = make_hammer(
+        &args.hammerer,
+        &pattern.pattern,
+        &pattern.mapping,
+        mem_config,
+        block_size,
+        &memory,
+        50,
+        false,
+    )?;
+    let each_attempt_hammerer = make_hammer(
+        &args.hammerer,
+        &pattern.pattern,
+        &pattern.mapping,
+        mem_config,
+        block_size,
+        &memory,
+        50,
+        true,
+    )?;
+    let aggressors = pattern.mapping.get_hammering_addresses_relocate(
+        &pattern.pattern.access_ids,
+        mem_config,
+        block_size.ilog2() as usize,
+        &memory,
+    )?;
+    let p = progress.add(ProgressBar::new(2000));
+    let pattern = DataPattern::Random(Box::new(StdRng::from_seed(rand::random())));
+    for (idx, hammerer) in [each_attempt_hammerer, hammerer].iter().enumerate() {
+        println!(
+            "{}",
+            if idx == 0 {
+                "Each attempt hammerer"
+            } else {
+                "Hammerer"
+            }
+        );
+        for _ in 0..args.repeat {
+            p.inc(1);
+            let start = std::time::Instant::now();
+            let mut victim = victim::MemCheck::new(&memory, pattern.clone());
+            let result = hammerer.hammer(&mut victim);
+            let duration = std::time::Instant::now() - start;
+            let bit_flips = match result {
+                Ok(result) => {
+                    info!(
+                        "Profiling hammering round successful at attempt {}: {:?}",
+                        result.attempt, result.victim_result
+                    );
+                    result.victim_result
+                }
+                Err(e) => {
+                    warn!("Profiling hammering round not successful: {:?}", e);
+                    vec![]
+                }
+            };
+            println!(
+                "{:?}",
+                RoundProfile {
+                    bit_flips,
+                    pattern: pattern.clone(),
+                    duration
+                }
+            );
+        }
+    }
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    unsafe { _main() }
+}
