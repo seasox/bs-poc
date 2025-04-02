400c8d145d438890f4f4fd8a36d93f2c101e0185
Jeremy Boy <je.boy@uni-luebeck.de>
2025-04-02 15:48:21 +0200

    refactor: remove unused functions in stack-dummy
    Those are implemented in memutils (get_phys) or
    not needed anymore (ReadBuf thread)

--- victims/stack-dummy/main.c
+++ victims/stack-dummy/main.c
@@ -20,68 +20,6 @@
 #define CL_SIZE 64     // Define the cache line size (typically 64 bytes)
 #define PAGEMAP_ENTRY_SIZE 8 // Each pagemap entry is 8 bytes
 
-// Function to get PFN from a virtual address
-uint64_t get_pfn_from_vaddr(uint64_t vaddr) {
-    uint64_t pfn = 0;
-    uint64_t page_offset = vaddr / PAGE_SIZE;
-    uint64_t pagemap_entry = 0;
-    char pagemap_path[64];
-    
-    // Open the /proc/self/pagemap file
-    snprintf(pagemap_path, sizeof(pagemap_path), "/proc/self/pagemap");
-    int pagemap_fd = open(pagemap_path, O_RDONLY);
-    if (pagemap_fd == -1) {
-        perror("Failed to open /proc/self/pagemap");
-        return -1;
-    }
-
-    // Seek to the pagemap entry corresponding to the virtual page
-    off_t offset = page_offset * PAGEMAP_ENTRY_SIZE;
-    if (lseek(pagemap_fd, offset, SEEK_SET) == (off_t)-1) {
-        perror("Failed to seek in /proc/self/pagemap");
-        close(pagemap_fd);
-        return -1;
-    }
-
-    // Read the pagemap entry for the virtual address
-    if (read(pagemap_fd, &pagemap_entry, PAGEMAP_ENTRY_SIZE) != PAGEMAP_ENTRY_SIZE) {
-        perror("Failed to read pagemap entry");
-        close(pagemap_fd);
-        return -1;
-    }
-
-    // Close the pagemap file
-    close(pagemap_fd);
-
-    // Check if the page is present in memory
-    if (!(pagemap_entry & (1ULL << 63))) {
-        fprintf(stderr, "Page not present in memory\n");
-        return -1;
-    }
-
-    // Extract the PFN from the pagemap entry (bits 0-54)
-    pfn = pagemap_entry & ((1ULL << 55) - 1);
-    return pfn;
-}
-
-struct ReadBufArgs {
-	unsigned char *buf;
-	size_t bufsize;
-};
-
-void* read_buffer(void *pargs) {
-	unsigned char *buf = ((struct ReadBufArgs*)pargs)->buf;
-	size_t bufsize = ((struct ReadBufArgs*)pargs)->bufsize;
-	while (1) {
-		for (size_t i = 0; i < bufsize; ++i) {
-			volatile int x;
-			asm("mov %1, %0" : "=r"(x) : "m"(buf[i]));
-			_mm_clflush(&buf[i]);
-		}
-	}
-	return NULL;
-}
-
 void sigusr1_handler(int signum) {
 	fprintf(stderr, "Received SIGUSR1, continuing...\n");
 }
@@ -92,22 +30,12 @@ int main(int argc, char **argv) {
 	setbuf(stdout, NULL);
 	setbuf(stderr, NULL);
 
-	uint64_t phy = get_pfn_from_vaddr((uint64_t)buf);
+	uint64_t phy = get_physical_address(buf);
 	fprintf(stderr, "%lx\n", phy);
 
 	// Wait for SIGINT before continuing
 	signal(SIGUSR1, sigusr1_handler);
 
-	// launch thread reading and flushing buf
-	//pthread_t thread_id;
-	//struct ReadBufArgs args = {
-	//	.buf = buf,
-	//	.bufsize = BUFSIZE
-	//};
-	//pthread_create(&thread_id, NULL, read_buffer, &args);
-	//printf("pattern:\n");
-	//fflush(stdout);
-	//unsigned char pattern = getchar();
 	unsigned char pattern[2] = {0b10101010, 0b01010101};
 	int fd = mtrr_open();
 	if (fd < 0) {
@@ -134,7 +62,6 @@ int main(int argc, char **argv) {
 		// todo inspect asm
 	}
 	mtrr_close(fd);
-	//pthread_cancel(thread_id);
 	return 0;
 }
 
