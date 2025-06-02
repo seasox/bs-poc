4c85a18b9937c9c5f2f06a473c313b2997570213
Jeremy Boy <je.boy@uni-luebeck.de>
2025-06-02 10:37:02 +0200

    print tsc in MEMUTILS_PRINT_OFFSET, add _LABELLED flavor

--- victims/lib/memutils/memutils.h
+++ victims/lib/memutils/memutils.h
@@ -11,9 +11,10 @@
 #define FLUSH(ptr) \
     __asm__ volatile("clflush (%0); mfence" :: "r"((ptr)) : "memory")
 
-
-#define MEMUTILS_PRINT_OFFSET(ptr, size) \
-    fprintf(stderr, #ptr "=%p,offset=%ld,phys=0x%lx,time=%llu,size=%ld\n", (void*)(ptr), get_stack_offset((ptr)), get_physical_address((ptr)), measure_access((ptr), 1), (long)(size))
+#define MEMUTILS_PRINT_OFFSET(ptr, size) MEMUTILS_PRINT_OFFSET_LABELLED("", ptr, size)
+    
+#define MEMUTILS_PRINT_OFFSET_LABELLED(label, ptr, size) \
+    fprintf(stderr, label "," #ptr "=%p,offset=%ld,phys=0x%lx,time=%llu,size=%ld,tsc=%lu\n", (void*)(ptr), get_stack_offset((ptr)), get_physical_address((ptr)), measure_access((ptr), 1), (long)(size), get_cycle_count())
 
 extern uint32_t fault_id;
 
@@ -30,4 +31,10 @@ int mtrr_open(void);
 int mtrr_page_uncachable(int fd, uint64_t phys);
 void mtrr_close(int fd);
 
+/* Inline function to read the 64-bit cycle counter */
+static inline uint64_t get_cycle_count(void) {
+    uint32_t lo, hi;
+    __asm__ volatile("rdtsc" : "=a"(lo), "=d"(hi));
+    return ((uint64_t)hi << 32) | lo;
+}
 #endif // MEMUTILS_H
